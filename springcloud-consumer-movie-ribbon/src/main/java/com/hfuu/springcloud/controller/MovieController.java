9fa5f7b234043287693523088c3e7796c2a8f8d2
sj <1215817408@qq.com>
2018-08-28 20:04:25 +0800

    把movie服务注册到eureka上

---
+++ springcloud-consumer-movie-ribbon/src/main/java/com/hfuu/springcloud/controller/MovieController.java
@@ -0,0 +1,28 @@
+package com.hfuu.springcloud.controller;
+
+import com.hfuu.springcloud.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+  @Autowired
+  private RestTemplate restTemplate;
+  @Autowired
+  private LoadBalancerClient loadBalancerClient;
+
+  @GetMapping("/movie/{id}")
+  public User findById(@PathVariable Long id) {
+    // http://localhost:7900/simple/
+    // VIP virtual IP
+    // HAProxy Heartbeat
+    return this.restTemplate.getForObject("http://springcloud-provider-user/user/getById/" + id, User.class);
+  }
+
+
+}
