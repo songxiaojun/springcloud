017bb8e0ba66240596ae24070cbbc5eb5cb0d27b
sj <1215817408@qq.com>
2018-08-28 15:53:14 +0800

    把movie服务注册到eureka上

--- springcloud-consumer-movie/src/main/java/com/hfuu/springcloud/SpringcloudConsumerMovieApplication.java
+++ springcloud-consumer-movie/src/main/java/com/hfuu/springcloud/SpringcloudConsumerMovieApplication.java
@@ -2,10 +2,12 @@ package com.hfuu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringcloudConsumerMovieApplication {
 	@Bean
 	public RestTemplate restTemplate() {
