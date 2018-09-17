786d117484d50a5c70a52f5fdcfe715bdff24d26
sj <1215817408@qq.com>
2018-09-17 23:33:49 +0800

    添加公共组件服务

---
+++ springcloud-common/src/main/java/com/hfuu/springcloud/SpringcloudCommonApplication.java
@@ -0,0 +1,12 @@
+package com.hfuu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudCommonApplication.class, args);
+	}
+}
