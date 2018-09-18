3368c319d957e38b98343907bbe3463e6299da95
sj <1215817408@qq.com>
2018-09-18 14:27:47 +0800

    mybatis换成mybatis-plus

--- springcloud-provider-user/src/main/java/com/hfuu/springcloud/SpringcloudProviderUserApplication.java
+++ springcloud-provider-user/src/main/java/com/hfuu/springcloud/SpringcloudProviderUserApplication.java
@@ -1,9 +1,10 @@
 package com.hfuu.springcloud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+@MapperScan("com.hfuu.springcloud.dao")
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SpringcloudProviderUserApplication {
