2c9ae5d01799f629a906bad5e15fa8dda0f6802e
sj <1215817408@qq.com>
2018-09-20 17:05:08 +0800

    添加两个拦截器

---
+++ springcloud-common/src/main/java/com/hfuu/springcloud/interceptors/RequestHandleInterceptor.java
@@ -0,0 +1,29 @@
+package com.hfuu.springcloud.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * sj
+ * 2018-09-20
+ * 请求处理拦截, 防止重放攻击
+ */
+public class RequestHandleInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
