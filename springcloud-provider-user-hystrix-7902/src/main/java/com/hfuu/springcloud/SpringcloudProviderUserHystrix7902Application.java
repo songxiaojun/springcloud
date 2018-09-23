555f09edce30aaa93a5190d43d11e0b08d31f187
sj <1215817408@qq.com>
2018-09-24 01:31:50 +0800

    添加hystrix工程

--- springcloud-provider-user-hystrix-7902/src/main/java/com/hfuu/springcloud/SpringcloudProviderUserHystrix7902Application.java
+++ springcloud-provider-user-hystrix-7902/src/main/java/com/hfuu/springcloud/SpringcloudProviderUserHystrix7902Application.java
@@ -4,9 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
 @MapperScan("com.hfuu.springcloud.dao")
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class SpringcloudProviderUserHystrix7902Application {
 
 	public static void main(String[] args) {
--- springcloud-provider-user-hystrix-7902/src/main/java/com/hfuu/springcloud/controller/UserController.java
+++ springcloud-provider-user-hystrix-7902/src/main/java/com/hfuu/springcloud/controller/UserController.java
@@ -2,14 +2,14 @@ package com.hfuu.springcloud.controller;
 
 import com.hfuu.springcloud.entity.User;
 import com.hfuu.springcloud.service.UserService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("/user")
+@RestController
 public class UserController {
     @Autowired
     private UserService userService;
@@ -22,9 +22,18 @@ public class UserController {
     }
 
     @GetMapping("/getById/{id}")
-    @ResponseBody
+    @HystrixCommand(fallbackMethod = "processHystrix_Get")
     public User getById(@PathVariable Integer id){
         User user = userService.getById(id);
+        if (null == user) {
+            throw new RuntimeException("该ID：" + id + "没有没有对应的信息");
+        }
         return user;
+    }
+
+    public User processHystrix_Get(@PathVariable("id") Integer id)
+    {
+        return new User().setId(id).setName("该ID：" + id + "没有没有对应的信息,null--@HystrixCommand")
+                .setDbSource("no this database in MySQL");
     }
 }
