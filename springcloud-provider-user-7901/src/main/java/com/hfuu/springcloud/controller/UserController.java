bf58ffa7e666e60db27a06aaf8847a0d4d55cfb9
sj <1215817408@qq.com>
2018-09-23 22:21:18 +0800

    服务提供者集群

---
+++ springcloud-provider-user-7901/src/main/java/com/hfuu/springcloud/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.hfuu.springcloud.controller;
+
+import com.hfuu.springcloud.entity.User;
+import com.hfuu.springcloud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @GetMapping("/list")
+    @ResponseBody
+    public List<User> list(){
+
+        List<User> list = userService.list();
+        return list;
+    }
+
+    @GetMapping("/getById/{id}")
+    @ResponseBody
+    public User getById(@PathVariable Integer id){
+        User user = userService.getBtId(id);
+        return user;
+    }
+}
---
+++ springcloud-provider-user-7901/src/main/java/com/hfuu/springcloud/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.hfuu.springcloud.service.impl;
+
+import com.hfuu.springcloud.dao.UserMapper;
+import com.hfuu.springcloud.entity.User;
+import com.hfuu.springcloud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+    @Override
+    public List<User> list() {
+        return userMapper.selectList(null);
+    }
+
+    @Override
+    public User getBtId(Integer id) {
+        return userMapper.getBtId(id);
+    }
+}
