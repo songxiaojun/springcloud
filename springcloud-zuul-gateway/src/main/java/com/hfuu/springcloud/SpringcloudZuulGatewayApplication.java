1aef85a0c74b43750dba8faab63fde1f12fe7b43
sj <1215817408@qq.com>
2018-09-25 21:30:33 +0800

    重新构建zuul项目

--- springcloud-zuul-gateway/src/main/java/com/hfuu/springcloud/SpringcloudZuulGatewayApplication.java
+++ springcloud-zuul-gateway/src/main/java/com/hfuu/springcloud/SpringcloudZuulGatewayApplication.java
@@ -1,13 +1,12 @@
 package com.hfuu.springcloud;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
-public class SpringcloudZuulApplication {
+public class SpringcloudZuulGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringcloudZuulApplication.class, args);
+		SpringApplication.run(SpringcloudZuulGatewayApplication.class, args);
 	}
 }
