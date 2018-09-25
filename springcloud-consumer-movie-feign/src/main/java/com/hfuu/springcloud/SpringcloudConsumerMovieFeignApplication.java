b04ce298f8fbee83bfcfa63f8b17827cdec995c9
sj <1215817408@qq.com>
2018-09-25 16:21:49 +0800

    修改controller中getMapping

--- springcloud-consumer-movie-feign/src/main/java/com/hfuu/springcloud/SpringcloudConsumerMovieFeignApplication.java
+++ springcloud-consumer-movie-feign/src/main/java/com/hfuu/springcloud/SpringcloudConsumerMovieFeignApplication.java
@@ -3,16 +3,20 @@ package com.hfuu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableFeignClients(basePackages = {"com.hfuu.springcloud"})
+@ComponentScan("com.hfuu.springcloud")
 public class SpringcloudConsumerMovieFeignApplication {
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudConsumerMovieFeignApplication.class, args);
 	}
--- springcloud-consumer-movie-feign/src/main/java/com/hfuu/springcloud/controller/MovieController.java
+++ springcloud-consumer-movie-feign/src/main/java/com/hfuu/springcloud/controller/MovieController.java
@@ -1,21 +1,19 @@
 package com.hfuu.springcloud.controller;
 
 import com.hfuu.springcloud.entity.User;
+import com.hfuu.springcloud.feign.UserClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/movie")
 public class MovieController {
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClientService userClientService;
 
-    @GetMapping("/findById/{id}")
-    public User findById(@PathVariable Integer id) {
-        return this.restTemplate.getForObject("http://localhost:7900/user/getById/"+id,User.class);
+    @RequestMapping(value = "/movie/getById/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public User findById(@PathVariable("id") Long id) {
+        User user = userClientService.getById(id);
+        return user;
     }
 }
