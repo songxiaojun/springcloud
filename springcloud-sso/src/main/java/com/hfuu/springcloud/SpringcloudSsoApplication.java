47b1d711d17ea8a2a34073dd1feefe97ef204206
sj <1215817408@qq.com>
2018-10-01 09:21:45 +0800

    单点登陆服务

---
+++ springcloud-sso/src/main/java/com/hfuu/springcloud/SpringcloudSsoApplication.java
@@ -0,0 +1,12 @@
+package com.hfuu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudSsoApplication.class, args);
+    }
+}
