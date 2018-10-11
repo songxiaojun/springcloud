9e95fd973683aefc2a43dc0df5bafff94730ab57
sj <1215817408@qq.com>
2018-10-11 14:12:33 +0800

    合并eureka

---
+++ springcloud-eureka/src/main/java/com/hfuu/springcloud/SpringcloudEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.hfuu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEurekaApplication.class, args);
+    }
+}
